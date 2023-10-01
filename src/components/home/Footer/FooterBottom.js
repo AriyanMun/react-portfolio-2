a7bcde77263848948fa9bec27c6d8ff080809107
AriyanMun <ariyanmunshi98@gmail.com>
2023-10-01 11:18:15 +0530

    first commit

---
+++ src/components/home/Footer/FooterBottom.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const FooterBottom = () => {
+  return (
+    <div className="w-full bg-[#ff8d8d] group">
+      <div className="max-w-container mx-auto border-t-[1px] pt-10 pb-20">
+        <p className="text-titleFont font-normal text-center flex md:items-center justify-center text-lightText duration-200 text-sm">
+          <span className="text-md mr-[1px] mt-[2px] md:mt-0 text-center hidden md:inline-flex">
+            
+          </span>
+          | Ariyan's shopping |
+
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FooterBottom;
